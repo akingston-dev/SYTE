bf08d5c6ddcb2c1530384063119376a586e0c819
AJ Kingston <akingstonlewis@gmail.com>
2022-03-13 15:47:51 -0700

    ad

---
+++ build/precache-manifest.767c786936b4b70c19087411b6bc34df.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05e8cb3c4ba5576dcf9a9bc1ea18da39",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0f524c38f1bdeb798d67",
+    "url": "./static/js/2.1a8c9f43.chunk.js"
+  },
+  {
+    "revision": "1e7ce0b5e5c5dbbe2385af5c63cacff7",
+    "url": "./static/js/2.1a8c9f43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "662d83396ed09d2fcb28",
+    "url": "./static/js/main.50f52dc5.chunk.js"
+  },
+  {
+    "revision": "4e681116cd9089599049",
+    "url": "./static/js/runtime-main.9904f276.js"
+  },
+  {
+    "revision": "79ba2002d56019901132986d5c84067a",
+    "url": "./static/media/profile.79ba2002.jpg"
+  },
+  {
+    "revision": "55cfb4817b9e054491bd9dafc21087c2",
+    "url": "./static/media/svg-1.55cfb481.svg"
+  },
+  {
+    "revision": "d4846e1163a89864dd49fb03ec6308ed",
+    "url": "./static/media/svg-2.d4846e11.svg"
+  },
+  {
+    "revision": "397c455a49d4f872c292a3dc25432579",
+    "url": "./static/media/svg-3.397c455a.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './pages/HomePage/Home';
 import Services from './pages/Services/Services';
 import Products from './pages/Products/Products';
 import SignUp from './pages/SignUp/SignUp';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop';
 import { Navbar, Footer } from './components';
 
